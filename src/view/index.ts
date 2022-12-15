9025f287fdb67198f9466bfbbf20a7a47cd76cd2
fogarea <shiitakedd@gmail.com>
2022-12-15 18:17:29 +0300

    feat: implement routing

--- src/view/index.ts
+++ src/view/index.ts
@@ -1,35 +1,90 @@
 import { ExtendedElement, Utils } from "../utils/utils";
 import { Controller } from "../controller/index";
 import { Model } from "../model/index";
-import { CounterView } from "./counter.view";
+import { Routing } from "types/routing";
+import { MainView } from "./main.view";
+import { ProductView } from "./product.view";
+import { CartView } from "./cart.view";
 
 export class View {
   root: ExtendedElement;
-  counterView: CounterView;
+  mainView: MainView;
+  productView: ProductView;
+  cartView: CartView;
 
   constructor(private controller: Controller, private model: Model) {
     this.root = Utils.id("#root") as ExtendedElement;
-    this.counterView = new CounterView(this.root, model, controller);
+    this.mainView = new MainView();
+    this.productView = new ProductView();
+    this.cartView = new CartView();
   }
 
   init() {
     this.render();
-    this.addHandlers();
+    // this.addHandlers();
     this.addListeners();
   }
 
   render() {
-    this.counterView.render();
+    this.renderHeader();
+    this.renderContent();
+    this.renderRoute();
   }
-
-  addHandlers() {
-    this.counterView.addHandlers();
-  }
+  /*
+    addHandlers() {
+    }
+  */
 
   addListeners() {
-    this.model.on("change.value", () => {
-      this.counterView.render();
-      this.counterView.addHandlers();
-    }); // subscribe
+    window.addEventListener("popstate", () => {
+      this.controller.updateRoute();
+    });
+
+    this.model.on("route", () => {
+      this.renderRoute();
+    });
+  }
+
+  renderRoute() {
+    const main = Utils.id(".main") as ExtendedElement;
+    console.log("this.model.route", this.model.route);
+    switch (this.model.route) {
+      case "":
+      case Routing.MAIN:
+        this.mainView.render(main);
+        break;
+      case Routing.CART:
+        this.cartView.render(main);
+        break;
+      default:
+        this.productView.render(main, this.model.productId);
+    }
+  }
+
+  renderHeader() {
+    const header = Utils.create<HTMLElement>("header", "header");
+    const mainLink = Utils.create<HTMLAnchorElement>("header__main-link", "a");
+    mainLink.href = "#";
+    mainLink.innerText = "Main";
+
+    const cartLink = Utils.create<HTMLAnchorElement>("header__cart-link", "a");
+    cartLink.href = "#cart";
+    cartLink.innerText = "Cart";
+
+    const productLink = Utils.create<HTMLAnchorElement>(
+      "header__product-link",
+      "a"
+    );
+    productLink.href = "#product-0";
+    productLink.innerText = "Product";
+
+    header.append(mainLink, cartLink, productLink);
+
+    this.root.append(header);
+  }
+
+  renderContent() {
+    const main = Utils.create<HTMLElement>("main", "main");
+    this.root.append(main);
   }
 }
