5ca63eced65c2e558dd5d811bc0a37ab9e45623f
Elijah <elijah.ivanik@inbox.ru>
2022-12-16 15:52:16 +0300

    refactor: split router model

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,39 +1,20 @@
-import { State } from "store/index";
 import { Observer } from "../utils/observer";
-import { SearchParams, SearchParamsObject } from "types/searchParams";
+import { RouterModel } from "./router.model";
 
 export class Model extends Observer {
+  routerModel: RouterModel;
+
   constructor() {
     super();
+    this.routerModel = new RouterModel();
   }
 
   setRoute(route: string) {
-    State.route = route;
+    this.routerModel.setRoute(route);
     this.emmit("route");
   }
 
   get route() {
-    const route = new URL(State.route);
-    const searchParams: SearchParamsObject = {};
-    const searchString: Array<string> = [];
-    const { host, origin } = route;
-
-    for (const param of Object.values(SearchParams)) {
-      const value = route.searchParams.get(param);
-
-      searchParams[param] = value;
-
-      if (value) {
-        searchString.push(`${param}=${value}`);
-      }
-    }
-
-    return {
-      host,
-      origin,
-      searchParams,
-      searchString: searchString.join("&"),
-      path: route.pathname.split("/")
-    };
+    return this.routerModel.route;
   }
 }
